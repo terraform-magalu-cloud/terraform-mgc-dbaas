5f1d01f52f28ac8f41a510163104b5de84b277d9
Antonio Novaes <antonionovaesjr@gmail.com>
2024-06-15 17:38:43 -0300

    chore: change name resource

--- examples/main.tf
+++ examples/main.tf
@@ -2,7 +2,7 @@ provider "mgc" {
   region = "br-ne1"
 }
 
-resource "random_string" "random" {
+resource "random_string" "sufix_name" {
   length  = 8
   special = false
 }
@@ -14,7 +14,7 @@ resource "random_string" "password" {
 
 module "dbaas-example" {
   source   = "../"
-  name     = "db-example-${random_string.random.id}"
+  name     = "db-example-${random_string.sufix_name.id}"
   flavor   = "cloud-dbaas-bs1.medium"
   engine   = "mysql-5.7"
   user     = "admin"
