23b9f84eb814ec1ec343e8d7a4fd42862cb2638d
Antonio Novaes <antonionovaesjr@gmail.com>
2024-06-15 17:35:54 -0300

    chore: order change variable

--- variables.tf
+++ variables.tf
@@ -15,78 +15,81 @@ variable "flavor" {
   type        = string
   default     = "cloud-dbaas-bs1.medium"
   validation {
-    condition = length(regexall("cloud-dbaas-(bs1|gp1|hm1).(small|medium|large)", var.flavor)) > 0
+    condition     = length(regexall("cloud-dbaas-(bs1|gp1|hm1).(small|medium|large)", var.flavor)) > 0
     error_message = "Value not found, possible values: cloud-dbaas-bs1.medium, cloud-dbaas-bs1.large, cloud-dbaas-bs1.small, cloud-dbaas-gp1.small, cloud-dbaas-gp1.medium,  cloud-dbaas-gp1.large, cloud-dbaas-hm1.medium, cloud-dbaas-hm1.large, cloud-dbaas-hm1.small"
+  }
+}
+
+variable "engine" {
+  type        = string
+  default     = "mysql-8.0"
+  description = "value"
+  validation {
+    condition     = length(regexall("mysql-(5.6|5.7|8.0)", var.engine)) > 0
+    error_message = "Value not found, possible values: mysq-5.6, mysql-5.7 and mysql-8.0"
   }
 }
 
 variable "user" {
   type        = string
-  description = "value"
+  description = "Define name firts user to login dbaas"
   default     = "admin"
 }
 
 variable "password" {
   type        = string
-  description = "value"
-  default     = "abc0123asdmc"
+  description = "Define password firt user to login dbaas"
+  sensitive   = true
 }
 
 variable "volume_size" {
   type        = number
-  description = "value"
-  default     = 10
+  description = "Define size volume in GB on dbaas"
+  default     = 20
 }
 
 variable "volume_type" {
   default     = "CLOUD_NVME_15K"
-  description = "value"
+  description = "Define type volume in GB on dbaas"
   type        = string
 }
 
 variable "backup_retention_days" {
   default     = 7
   type        = number
-  description = "value"
+  description = "Define days to backup frequency of the dbaas"
 }
 
 variable "backup_start_at" {
   type        = string
   default     = "02:00:00"
-  description = "value"
+  description = "Define hour start backup of the dbaas"
 }
 
 variable "backup_enable" {
   type        = bool
   default     = true
-  description = "value"
+  description = "Define if backup is enable of the dbaas"
 }
 
 variable "exchange" {
   default     = "dbaas-internal"
   type        = string
-  description = "value"
+  description = "Define exchange type"
 }
 
 variable "replicas_enable" {
   type        = bool
-  default     = true
-  description = "value"
+  default     = false
+  description = "Define if this dbaas use replica"
 }
 
 variable "replicas_num_hosts" {
   type        = number
-  default     = 2
-  description = "value"
-}
-
-
-variable "engine" {
-  type = string
-  default = "mysql-8.0"
-  description = "value"
+  default     = 1
+  description = "Define number hosts to be replica thiss dbaas"
   validation {
-    condition = length(regexall("mysql-(5.6|5.7|8.0)",var.engine)) > 0 
-    error_message = "Value not found, possible values: mysq-5.6, mysql-5.7 and mysql-8.0"
+    condition     = var.replicas_num_hosts < 1
+    error_message = "This value can1t less than 1"
   }
 }
